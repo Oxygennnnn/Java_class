a5f91cd716162eebd36fa4b2730235ee6f20dee3
HAOYANG LI <h.li2.22@abdn.ac.uk>
2023-11-09 11:23:34 +0000

    collatz

--- Practical5/Collatz.java
+++ Practical5/Collatz.java
@@ -4,19 +4,30 @@ public class Collatz{
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter a number:");
         int result=scanner.nextInt();
-        printCollatzSequence(result);
+        try{
+            int step=printCollatzSequence(result);
+            System.out.println("Numbers of steps:"+step);
+        }catch(IllegalArgumentException e){
+            System.out.println("Please use valid input.");
+        }
     }
     public static int printCollatzSequence(int result){
+        int n=0;
+        if (result<=0){
+            throw new IllegalArgumentException("Please enter integer.");
+        }
             if (result==1){
                 return 1;
             }else if (result%2==1){
                 result=3*result+1;
                 System.out.println(result);
-                return printCollatzSequence(result);
+                n+=1;
+                return n+printCollatzSequence(result);
             }else if (result%2==0){
                 result=result/2;
                 System.out.println(result);
-                return printCollatzSequence(result);
+                n+=1;
+                return n+printCollatzSequence(result);
             }else {
                 System.out.println("Invalid input.");
                 return -1;
